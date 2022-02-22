e5abf968fdf402cc2fc9ff3272d35aea2a8dd62f
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-02-22 12:42:53 -0300

    feat: adicionado contador com while

--- src/br/fai/ex04/Main.java
+++ src/br/fai/ex04/Main.java
@@ -1,5 +1,16 @@
 package br.fai.ex04;
 
 public class Main {
-
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+	}
+	
+	private void start(){
+		int i = 0;
+		while (i < 20) {
+			i += 1;
+			System.out.println("Valor i: " + i);
+		}
+	}
 }
